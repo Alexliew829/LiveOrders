456ff61b24f1f57a8a80e606082f4076993618f9
Alexliew829 <alexliewlla@gmail.com>
2025-06-12 02:34:44 +0800

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    message: "Hello from /api/products!!!",
-    timestamp: new Date().toISOString(),
+    message: "API is working!",
+    timestamp: new Date().toISOString()
   });
 }
