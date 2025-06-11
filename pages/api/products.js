365c8198a98ec55f6195b8da16d480f9367ad61e
Alexliew829 <alexliewlla@gmail.com>
2025-06-12 01:21:56 +0800

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,3 +1,4 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({ message: "API /products is working!" });
+// pages/api/products.js
+module.exports = (req, res) => {
+  res.status(200).json({ message: "API is working!" });
 }
